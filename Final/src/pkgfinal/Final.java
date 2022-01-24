0b0b3e633bb9b679598e9a52f8c1d502676b21bc
adrialva <58445399+adrialva@users.noreply.github.com>
2022-01-24 15:42:35 -0500

    Add files via upload

---
+++ Final/src/pkgfinal/Final.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgfinal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Logica.ConexionBD;
+import Logica.MedicamentosSolidos;
+import java.util.List;
+
+/*
+ *
+ * @author ADRIANA
+ */
+public class Final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ConexionBD conexion = new ConexionBD();
+        MedicamentosSolidos solidos1 = new MedicamentosSolidos();
+        Vista fin = new Vista();
+//        }
+//        
+    }
+// TODO code application logic here
+}
