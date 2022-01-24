6402397c07c6997896d185e582c3e660595fcc7c
adrialva <58445399+adrialva@users.noreply.github.com>
2022-01-24 15:13:38 -0500

    Add files via upload

---
+++ Proyecto_version/src/prrincipal/Estudiante.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prrincipal;
+
+/**
+ *
+ * @author ADRIANA
+ */
+public class Estudiante {
+    private int edad;
+    
+    public Estudiante(int edad){
+        this.edad=edad;
+    }
+}
---
+++ Proyecto_version/src/prrincipal/Proyecto_version.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prrincipal;
+
+/**
+ *
+ * @author ADRIANA
+ */
+public class Proyecto_version {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Esta es la segunda version");    
+// TODO code application logic here
+    }
+    
+}
